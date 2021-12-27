6ca4677b59785fc5e148096ab30d1c370b37ed3b
Anish Karn <anish.karna133@gmail.com>
2021-12-27 22:51:20 +0545

    add restframework and requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third party apps
+    'rest_framework',
+
+    # custom apps
     'newsfeed',
     'users'
 ]
